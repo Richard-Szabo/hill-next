8240d28864925dac45c68cc23ced390f8d9def6a
unknown <szaborichard07@gmail.com>
2025-09-03 22:03:49 +0200

    Fb pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header/Header";
 import { Karla, Merriweather } from "next/font/google";
 import GTM from "@/components/Google/GTM";
 import GTMTracker from "@/components/Google/GTMTracker";
+import Script from "next/script";
 
 const karla = Karla({
   subsets: ["latin"],
@@ -23,36 +24,59 @@ const merriweather = Merriweather({
 export const metadata: Metadata = {
   metadataBase: new URL("https://hillmedical.hu"),
   title: "Hill Medical",
-  description: 'Modern és fájdalommentes fogászati kezelések Győrújbaráton kicsiknek és nagyoknak egyaránt. Tapasztalt orvosaink segítenek mosolya megőrzésében.',
-  keywords: ['fogászat', 'győrújbarát', 'családi fogászat', 'digitális fogászat', 'győr', "Bölcsességfog", "Fogápolási módszerek", "Fogszabályozás", "Implantátum", "Inlay", "Láthatatlan sínek", "Rögzített fogszabályozás", "Természetes tömések", "Vérző fogíny", "X-Guide", "Ínybetegségek", "Mozgó fogak", "Szájhigénia", "Fájdalommentes fogkőszedés", "Gyermekfogászat"],
-  authors: [{ name: 'Hill Medical' }],
-  creator: 'Hill Medical',
+  description:
+    "Modern és fájdalommentes fogászati kezelések Győrújbaráton kicsiknek és nagyoknak egyaránt. Tapasztalt orvosaink segítenek mosolya megőrzésében.",
+  keywords: [
+    "fogászat",
+    "győrújbarát",
+    "családi fogászat",
+    "digitális fogászat",
+    "győr",
+    "Bölcsességfog",
+    "Fogápolási módszerek",
+    "Fogszabályozás",
+    "Implantátum",
+    "Inlay",
+    "Láthatatlan sínek",
+    "Rögzített fogszabályozás",
+    "Természetes tömések",
+    "Vérző fogíny",
+    "X-Guide",
+    "Ínybetegségek",
+    "Mozgó fogak",
+    "Szájhigénia",
+    "Fájdalommentes fogkőszedés",
+    "Gyermekfogászat",
+  ],
+  authors: [{ name: "Hill Medical" }],
+  creator: "Hill Medical",
   openGraph: {
-    title: 'Hill Medical - Családi fogászat Győrújbaráton',
-    description: 'Modern és fájdalommentes fogászati kezelések Győrújbaráton kicsiknek és nagyoknak egyaránt. Tapasztalt orvosaink segítenek mosolya megőrzésében.',
-    url: 'https://hillmedical.hu',
-    siteName: 'Hill Medical',
+    title: "Hill Medical - Családi fogászat Győrújbaráton",
+    description:
+      "Modern és fájdalommentes fogászati kezelések Győrújbaráton kicsiknek és nagyoknak egyaránt. Tapasztalt orvosaink segítenek mosolya megőrzésében.",
+    url: "https://hillmedical.hu",
+    siteName: "Hill Medical",
     images: [
       {
-        url: '/rendelo.jpg',
+        url: "/rendelo.jpg",
         width: 1000,
         height: 667,
-        alt: 'Hill Medical rendelő',
+        alt: "Hill Medical rendelő",
       },
     ],
-    locale: 'hu_HU',
-    type: 'website',
+    locale: "hu_HU",
+    type: "website",
   },
   icons: {
-    icon: '/logo.svg',
-    shortcut: '/logo.svg',
+    icon: "/logo.svg",
+    shortcut: "/logo.svg",
   },
 };
 
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1.0,
-}
+};
 
 export default function RootLayout({
   children,
@@ -66,13 +90,38 @@ export default function RootLayout({
         <GTM />
       </head>
       <body>
-         {/* GTM noscript in body */}
-         <noscript>
+        {/* GTM noscript in body */}
+        {/* Meta Pixel Code */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1110760790528718');
+          fbq('track', 'PageView');
+        `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1110760790528718&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+        <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-P4XL6WCH"
             height="0"
             width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
+            style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
         <GTMTracker />
